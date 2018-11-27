94ff1ddd44fcd1bbcaf6e3560a7e8f13d37ef093
Oranit <Oranit@Oranit-HP>
2018-11-27 09:44:53 +0200

    updae 1.0.1
    editting csv2km

--- src/File_format/csv2km.java
+++ src/File_format/csv2km.java
@@ -4,11 +4,13 @@ package File_format;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class csv2km {
 
     public static void main(String[] args) 
     {
+    	ArrayList<String> metaDeta = new ArrayList<String>();
         String csvFile = "WigleWifi_20171201110209.csv";
         String line = "";
         String cvsSplitBy = ",";
@@ -18,7 +20,6 @@ public class csv2km {
             while ((line = br.readLine()) != null) 
             {
                 String[] userInfo = line.split(cvsSplitBy);	
-                System.out.println(line);
             }
 
         } catch (IOException e) 
---
+++ src/GIS/metaData.java
@@ -0,0 +1,22 @@
+package GIS;
+
+import Geom.Point3D;
+
+public class metaData implements Meta_data {
+	/** returns the Universal Time Clock associated with this data; */
+
+	@Override
+	public long getUTC() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	/**
+	 * @return the orientation: yaw, pitch and roll associated with this data;
+	 */
+	@Override
+	public Point3D get_Orientation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
