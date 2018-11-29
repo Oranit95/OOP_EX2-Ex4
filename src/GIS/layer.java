be6559af43829c1123418df7a30c2f6c80ddbf08
טל <talnoamb@gmail.com>
2018-11-29 15:02:02 +0200

    1.0.4 update layer.

--- src/GIS/layer.java
+++ src/GIS/layer.java
@@ -10,9 +10,11 @@ public class layer extends LinkedHashSet<GIS_element> {
 	private Date date;
 
 
+	@SuppressWarnings("deprecation")
 	public layer() {
 		date = new Date();
-		data = new metaData("not yet complete");
+		String time = String.valueOf(date.getDate())+"/"+String.valueOf(date.getMonth())+"/"+String.valueOf(date.getYear())+" "+String.valueOf(date.getHours())+":"+String.valueOf(date.getMinutes())+":"+String.valueOf(date.getSeconds());
+		data = new metaData(time);
 	}
 	
 	public Meta_data get_Meta_data() {
