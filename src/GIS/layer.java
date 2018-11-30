a7d153869fcd815bedd250ae81cc28bf89420b26
Oranit <Oranit@Oranit-HP>
2018-11-30 17:11:19 +0200

    update 1.0.5
    editing csv reader and kml writer

--- src/GIS/layer.java
+++ src/GIS/layer.java
@@ -1,11 +1,14 @@
 package GIS;
 
-import java.util.LinkedHashSet;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 
-public class layer extends LinkedHashSet<GIS_element> {
+public class layer extends ArrayList<element> {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Meta_data data;
 	private Date date;
 
@@ -16,9 +19,27 @@ public class layer extends LinkedHashSet<GIS_element> {
 		String time = String.valueOf(date.getDate())+"/"+String.valueOf(date.getMonth())+"/"+String.valueOf(date.getYear())+" "+String.valueOf(date.getHours())+":"+String.valueOf(date.getMinutes())+":"+String.valueOf(date.getSeconds());
 		data = new metaData(time);
 	}
-	
+
 	public Meta_data get_Meta_data() {
 		return data;
+	}
+
+	public void reader(String path) {
+		String csvFile = path;
+		String line = "";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) 
+		{
+			while ((line = br.readLine()) != null) 
+			{
+				String[] data = line.split(",");
+				this.add(new element(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10]));	
+			}
+
+		} catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 	}
 
 }
