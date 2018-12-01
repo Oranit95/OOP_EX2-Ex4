283a807fd0bcd82869fff97115edba792741c1c2
Oranit <Oranit@Oranit-HP>
2018-12-01 22:29:57 +0200

    update 1.0.9
    adding doc to multycsv
    java doc is not working, need to be fixed. besides that, all done.
    
    
    Signed-off-by: Oranit <Oranit@Oranit-HP>

--- src/Algorithms/MultyCSV.java
+++ src/Algorithms/MultyCSV.java
@@ -3,15 +3,25 @@ import GIS.*;
 import java.io.File;
 
 import File_format.csv2kml;
-
+/**
+ * A class that scanns a directory and all it's sub-directorys to find csv files,
+ *than turn them into kml files. In addition, adding each csv file to a layer, and each layer to a project.
+ *
+ * @author Oranit
+ *
+ */
 public class MultyCSV {
 		project project;
-		File file;
 
 	public MultyCSV() {
 		project = new project();
 	}
-	
+	/**
+	 * in order to read multiple csv files, the user must create a file first with the requested directory,
+	 *  and than send the file itself to findFile().
+	 * @param dir
+	 * @return the directory that was send
+	 */
 	public File findFile(File dir) {
 		  File result = null; 
 		  File[] dirlist = dir.listFiles();
