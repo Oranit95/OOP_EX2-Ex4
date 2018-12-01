a7023e8e3a46fcc9d33e2de836f18a30208cb979
טל <talnoamb@gmail.com>
2018-12-01 12:43:55 +0200

    1.0.5 update

--- src/File_format/csv2kml.java
+++ src/File_format/csv2kml.java
@@ -3,12 +3,17 @@ package File_format;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Iterator;
 
-public class kmlPrinter 
+import GIS.*;
+import Geom.Point3D;
+
+public class csv2kml 
 {
-	public static void main(String[] args)
-	{
-		String fileName = "output.kml";
+	public void kml(String name, layer layer) {
+		
+		
+		String fileName = name;
 		PrintWriter pw = null;
 		try 
 		{
@@ -19,25 +24,30 @@ public class kmlPrinter
 			e.printStackTrace();
 			return;
 		}
-
-		StringBuilder sb = new StringBuilder();
-		sb.append("id");
-		sb.append(',');
-		sb.append("Name");
-		sb.append('\n');
-		
-		sb.append("1");
-		sb.append(',');
-		sb.append("Israel Israeli");
-		sb.append('\n');
-
-		pw.write(sb.toString());
-		pw.close();
-		System.out.println("done!");
+		Iterator<element> it = layer.iterator();
+		while(it.hasNext()) {
+			element gis = it.next();
+			metaData mD = (metaData) gis.getData();
+			Point3D pD = (Point3D) gis.getGeom();
+			StringBuilder sb = new StringBuilder();
+			sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n" + "");
+			sb.append("<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Style"
+					+ " id=\"red\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/red-dot.png</href></Icon></IconStyle></Style><Style "
+					+ "id=\"yellow\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/yellow-dot.png</href></Icon></IconStyle></Style><Style"
+					+ " id=\"green\"><IconStyle><Icon><href>http://maps.google.com/mapfiles/ms/icons/green-dot.png</href></Icon></IconStyle></Style><Folder><name>Wifi Networks</name>\r\n" + "");
+			sb.append("<Placemark>");
+			sb.append("\n");
+			sb.append("<name><![CDATA["+gis.getOtherData().SSID()+"]]></name>\n");	
+			sb.append("<description><![CDATA[BSSID: <b>"+gis.getOtherData().MAC()+"</b><br/>");
+			sb.append("Capabilities: <b>"+gis.getOtherData().AuthMode()+"</b><br/>");
+			sb.append("Frequency: <b>"+gis.getOtherData().RSSI()+"</b><br/>");
+			sb.append("Timestamp: <b>"+gis.getData().getUTC()+"</b><br/>");
+			sb.append("Date: <b>"+mD.FirstSeen()+"</b>]]></description><styleUrl>#red</styleUrl>\n");
+			sb.append("<Point>\n");
+			sb.append("<coordinates>"+pD.x()+","+pD.y()+"</coordinates></Point>\n");
+			sb.append("/Placemark");
+			pw.write(sb.toString());
+			pw.close();
+		}
 	}
 }
-
-
-
-
-
